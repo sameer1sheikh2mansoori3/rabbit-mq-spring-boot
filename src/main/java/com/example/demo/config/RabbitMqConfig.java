5f3b0fcb8f4b92d380d49090c7707366cc6ca7ee
sameer.mansoori <sameer.mansoori@addverb.com>
2025-11-14 10:35:01 +0530

    commit: lecture 9

---
+++ src/main/java/com/example/demo/config/RabbitMqConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing_key.name}")
+    private String routingKey;
+
+//    spring been for rabbit mq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+  @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+//    binding between queue and exchange using rabbitmq key
+
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
