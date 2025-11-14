f6be4f110fe3b945ff9566ec3d7366f2109fbeef
sameer.mansoori <sameer.mansoori@addverb.com>
2025-11-14 11:49:21 +0530

    commit: lecture 12 making consumer

---
+++ src/main/java/com/example/demo/consumer/RabbitMqConsumer.java
@@ -0,0 +1,16 @@
+package com.example.demo.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMqConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consumeMessage(String message){
+        logger.info(String.format("Message received -> %s", message));
+    }
+}
